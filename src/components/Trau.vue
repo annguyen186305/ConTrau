aa2061e2150ee99ac4d96c112909fbc05db28c87
annguyen186305 <annguyen186305@gmail.com>
2024-05-23 00:50:43 +0700

    Thay doi con trau

--- src/components/Trau.vue
+++ src/components/Trau.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>{{mota}}</p>
+    <p>{{mota }}</p>
     Trâu (còn gọi là trâu nước) là một loài động vật thuộc họ Trâu bò (Bovidae), phân bộ Nhai lại (Ruminantia), nhóm sừng rỗng (Cavicornes), bộ Guốc chẵn (Actiodactyla), lớp Thú có vú (Mammalia). Chúng sống hoang dã ở Nam Á, Đông Nam Á, miền bắc Úc
     <ConNghe duongdan="https://png.pngtree.com/png-vector/20230902/ourmid/pngtree-cute-cow-cartoon-clipart-png-image_9637644.png"/>
     <ConNghe duongdan="https://png.pngtree.com/png-vector/20230902/ourmid/pngtree-cute-cow-cartoon-clipart-png-image_9637644.png"/>
